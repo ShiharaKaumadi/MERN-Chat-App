6750779f031d9e5b79b14c0658abec7cd4566af5
ShiharaKaumadi <eshani9544@gmail.com>
2024-07-15 20:35:59 +0530

    Initialize Routes

---
+++ backend/routes/auth_routes.js
@@ -0,0 +1,6 @@
+import express from "express";
+const router = express.Router();
+router.get("/login",(req,res)=>res.send('login route'));
+router.get("/logout",(req,res)=>res.send('logout route'));
+router.get("/signup",(req,res)=>res.send('signup route'));
+export default router;
