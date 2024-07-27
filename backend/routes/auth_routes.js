309d1361c9d4c47a694acd13259395abac590f29
ShiharaKaumadi <eshani9544@gmail.com>
2024-07-27 12:47:39 +0530

    Save New User

--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -1,6 +1,13 @@
 import express from "express";
 const router = express.Router();
+import {login,logout,signup} from "../controllers/auth.controller.js";
+/*
 router.get("/login",(req,res)=>res.send('login route'));
 router.get("/logout",(req,res)=>res.send('logout route'));
 router.get("/signup",(req,res)=>res.send('signup route'));
+*/
+router.post("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
+
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from "./routes/auth_routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 const app = express();
-dotenv.config()
+
 const PORT = process.env.PORT||5000;
+dotenv.config();
+app.use(express.json());
+app.use("/api/auth",authRoutes);
+/*
 app.get("/",(req,res)=>res.send('Hello World'));
-app.listen(PORT,()=>console.log(`Server Running on Port ${PORT}`));
+*/
 /*
 app.get("api/auth/signup",(req, res)=>console.log("signup route"));
 app.get("api/auth/login",(req, res)=>console.log("login route"));
@@ -14,4 +19,8 @@ app.get("api/auth/signup",(req, res)=>console.log("signup route"));
 app.get("api/auth/login",(req, res)=>console.log("login route"));
 app.get("api/auth/logout",(req, res)=>console.log("logout route"));
 */
-app.use("/api/auth",authRoutes);
+
+app.listen(PORT,()=>{
+    connectToMongoDB();
+    console.log(`Server Running on Port ${PORT}`)
+});
